a7daa7b1fb0fe5cc8ce3ac88dc9fca2afab22cd0
M4V3R1CK <eddymav247@gmail.com>
2021-11-05 12:13:02 +0100

    page structure

--- pages/index.js
+++ pages/index.js
@@ -39,63 +39,279 @@ export default function Home() {
         </div>
       </nav>
 
-      <main class="flex flex-row mt-20 mb-50">
-        <div class="w-2/5 flex flex-col justify-center p-4">
-          <div class="flex flex-row items-center mb-6">
-            <div class="bg-gray-900 h-1 w-14 mr-4"></div>
-            <h2 class="text-3xl text-gray-500"> Office Space</h2>
-          </div>
-          <h2 class="font-sans-serif font-bold text-7xl text-gray-600 mb-8">
-            The New <br/> Way to Discover
-          </h2>
-          <p class="text-2xl text-gray-600 tracking-wide mb-8">
-            Booking Company-Wide Savings, Invoicing <br /> And Reporting Docs
-          </p>
+      <main class="flex flex-col mt-20 mb-50">
+        <div class="flex flex-row">
+          <div class="w-2/5 flex flex-col justify-center p-4">
+            <div class="flex flex-row items-center mb-6">
+              <div class="bg-gray-900 h-1 w-14 mr-4"></div>
+              <h2 class="text-3xl text-gray-500"> Office Space</h2>
+            </div>
+            <h2 class="font-sans-serif font-bold text-7xl text-gray-600 mb-8">
+              The New <br /> Way to Discover
+            </h2>
+            <p class="text-2xl text-gray-600 tracking-wide mb-8">
+              Booking Company-Wide Savings, Invoicing <br /> And Reporting Docs
+            </p>
 
-          <div class="flex flex-row items-center">
-            <a
-              href="#"
-              class=" bg-gray-900 rounded-lg py-4 px-6 text-gray-50 uppercase text-xl tracking-wide self-start my-5"
-            >
-              Book Demo
-            </a>
-            <button class=" flex flex-row items-center ml-3 text-xl hover:bg-gray-300 p-2 rounded-lg">
-              Our Showreel{" "}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-10 w-10 ml-2"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+            <div class="flex flex-row items-center">
+              <a
+                href="#"
+                class=" bg-gray-900 rounded-lg py-4 px-6 text-gray-50 uppercase text-xl tracking-wide self-start my-5"
               >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-                />
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            </button>
+                Book Demo
+              </a>
+              <button class=" flex flex-row items-center ml-3 text-xl hover:bg-gray-300 p-2 rounded-lg">
+                Our Showreel{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-10 w-10 ml-2"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div class="w-3/5">
+            <img src="/undraw_moving_re_pipp.svg" class="w-full" />
           </div>
         </div>
-        <div class="w-3/5">
-          <img src="/undraw_moving_re_pipp.svg" class="w-full" />
+        <div class="mx-auto text-6xl mt-20">
+          <p>Explore Events Around You </p>
+        </div>
+        <div>
+          <section class="text-gray-600 body-font">
+            <div class="container px-5 py-20 mx-auto">
+              <div class="flex flex-wrap -m-4">
+                <div class="p-4 md:w-1/3">
+                  <div class="h-100 border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                    <img
+                      class="lg:h-48 md:h-36 w-full object-cover object-center"
+                      src="https://dummyimage.com/720x400"
+                      alt="blog"
+                    />
+                    <div class="p-6">
+                      <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                        CATEGORY
+                      </h2>
+                      <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                        The Catalyzer
+                      </h1>
+                      <p class="text-xl mb-3">
+                        Photo booth fam kinfolk cold-pressed sriracha leggings
+                        jianbing microdosing tousled waistcoat.
+                      </p>
+                      <div class="flex items-center flex-wrap ">
+                        <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
+                          Learn More
+                          <svg
+                            class="w-4 h-4 ml-2"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          >
+                            <path d="M5 12h14"></path>
+                            <path d="M12 5l7 7-7 7"></path>
+                          </svg>
+                        </a>
+                        <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                          <svg
+                            class="w-4 h-4 mr-1"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                            <circle cx="12" cy="12" r="3"></circle>
+                          </svg>
+                          1.2K
+                        </span>
+                        <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                          <svg
+                            class="w-4 h-4 mr-1"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                          </svg>
+                          6
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="p-4 md:w-1/3">
+                  <div class="h-100 border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                    <img
+                      class="lg:h-48 md:h-36 w-full object-cover object-center"
+                      src="https://dummyimage.com/721x401"
+                      alt="blog"
+                    />
+                    <div class="p-6">
+                      <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                        CATEGORY
+                      </h2>
+                      <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                        The 400 Blows
+                      </h1>
+                      <p class="text-xl mb-3">
+                        Photo booth fam kinfolk cold-pressed sriracha leggings
+                        jianbing microdosing tousled waistcoat.
+                      </p>
+                      <div class="flex items-center flex-wrap">
+                        <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
+                          Learn More
+                          <svg
+                            class="w-4 h-4 ml-2"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          >
+                            <path d="M5 12h14"></path>
+                            <path d="M12 5l7 7-7 7"></path>
+                          </svg>
+                        </a>
+                        <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                          <svg
+                            class="w-4 h-4 mr-1"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                            <circle cx="12" cy="12" r="3"></circle>
+                          </svg>
+                          1.2K
+                        </span>
+                        <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                          <svg
+                            class="w-4 h-4 mr-1"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                          </svg>
+                          6
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="p-4 md:w-1/3">
+                  <div class="h-100 border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                    <img
+                      class="lg:h-48 md:h-36 w-full object-cover object-center"
+                      src="https://dummyimage.com/722x402"
+                      alt="blog"
+                    />
+                    <div class="p-6">
+                      <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                        CATEGORY
+                      </h2>
+                      <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                        Shooting Stars
+                      </h1>
+                      <p class="text-xl mb-3">
+                        Photo booth fam kinfolk cold-pressed sriracha leggings
+                        jianbing microdosing tousled waistcoat.
+                      </p>
+                      <div class="flex items-center flex-wrap ">
+                        <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
+                          Learn More
+                          <svg
+                            class="w-4 h-4 ml-2"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          >
+                            <path d="M5 12h14"></path>
+                            <path d="M12 5l7 7-7 7"></path>
+                          </svg>
+                        </a>
+                        <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                          <svg
+                            class="w-4 h-4 mr-1"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                            <circle cx="12" cy="12" r="3"></circle>
+                          </svg>
+                          1.2K
+                        </span>
+                        <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                          <svg
+                            class="w-4 h-4 mr-1"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                          </svg>
+                          6
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
       </main>
 
-    {/* <--footer --> */}
-      <footer class="flex flex-row justify-between mx-auto items-center mt-60 p-16">
+      {/* <--footer --> */}
+      <footer class="flex flex-row justify-between mx-auto items-center mt-30 p-16">
         <div class="flex flex-row w-3/5 items-center">
           <div class="bg-black w-16 h-16 rounded-full mr-5"></div>
           <div>
             <p class="text-xl font-bold mt-2">Question ?</p>
-            <a href="#" class="text-xl font-bold ">Talk to Our Experts</a>
+            <a href="#" class="text-xl font-bold ">
+              Talk to Our Experts
+            </a>
           </div>
         </div>
         <div>
@@ -108,7 +324,7 @@ export default function Home() {
               <a href="#">LinkedIn</a>
             </li>
             /
-            <li class="ml-2" >
+            <li class="ml-2">
               <a href="#">Instagram</a>
             </li>
           </ul>
